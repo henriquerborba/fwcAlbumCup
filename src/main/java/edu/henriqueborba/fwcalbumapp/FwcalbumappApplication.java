d16dd21017f85576ed5558863e2fefe82428d4ec
henriquerborba <henriquedeborba@hotmail.com>
2023-01-02 09:24:56 -0300

    First commit

---
+++ src/main/java/edu/henriqueborba/fwcalbumapp/FwcalbumappApplication.java
@@ -0,0 +1,13 @@
+package edu.henriqueborba.fwcalbumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwcalbumappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwcalbumappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/henriqueborba/fwcalbumapp/FwcalbumappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.henriqueborba.fwcalbumapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwcalbumappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
