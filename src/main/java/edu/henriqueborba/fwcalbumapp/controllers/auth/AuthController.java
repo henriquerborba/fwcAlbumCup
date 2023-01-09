27eb5ec42ae0548840b607725a7b14b3eb9fe8ff
henriquerborba <henriquedeborba@hotmail.com>
2023-01-09 15:59:19 -0300

    Documentação dos métodos criados

--- src/main/java/edu/henriqueborba/fwcalbumapp/controllers/auth/AuthController.java
+++ src/main/java/edu/henriqueborba/fwcalbumapp/controllers/auth/AuthController.java
@@ -5,7 +5,9 @@ import edu.henriqueborba.fwcalbumapp.dtos.auth.AuthenticationResponse;
 import edu.henriqueborba.fwcalbumapp.dtos.auth.RegisterRequest;
 import edu.henriqueborba.fwcalbumapp.models.User;
 import edu.henriqueborba.fwcalbumapp.services.auth.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,20 +16,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/")
 @RequiredArgsConstructor
+@Tag(name = "Auth")
 public class AuthController {
 
     private final AuthenticationService service;
 
+    @Operation(summary = "Registrar usuário no sistema")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@Validated @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
+    @Operation(summary = "Fazer o login e pegar o token de autorização")
     @PostMapping("/auth")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
+    @Operation(summary = "Recuperar informações do usuário logado")
     @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/me")
     public ResponseEntity<User> me() {
--- src/main/java/edu/henriqueborba/fwcalbumapp/controllers/sticker/StickerController.java
+++ src/main/java/edu/henriqueborba/fwcalbumapp/controllers/sticker/StickerController.java
@@ -3,7 +3,9 @@ package edu.henriqueborba.fwcalbumapp.controllers.sticker;
 import edu.henriqueborba.fwcalbumapp.dtos.sticker.StickerRequest;
 import edu.henriqueborba.fwcalbumapp.models.Sticker;
 import edu.henriqueborba.fwcalbumapp.services.sticker.StickerService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +20,18 @@ import java.util.List;
 @RequestMapping("/api/sticker")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Sticker")
 public class StickerController {
 
     final StickerService service;
 
+    @Operation(summary = "Listar todas as figurinhas do sistema.")
     @GetMapping
     public ResponseEntity<List<Sticker>> getAllSticker() {
         return ResponseEntity.ok(service.getAllSticker());
     }
 
+    @Operation(summary = "Adicionar figurinhas ao sistema")
     @PostMapping
     public ResponseEntity<Sticker> createSticker(@RequestBody StickerRequest sticker) {
         return ResponseEntity.ok(service.createSticker(sticker));
